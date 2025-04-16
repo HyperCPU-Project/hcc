8e183e24bbc15e4725651c4b1a0f03712d9f5e5d
aceinetx <mouselin82@gmail.com>
2025-04-16 16:01:03 +0300

    Revert b0a091c

--- src/result.hpp
+++ src/result.hpp
@@ -2,9 +2,6 @@
 
 #include <pch.hpp>
 
-// Forward declaration of void specialization
-template <typename E> class Result<void, E>;
-
 template <typename T, typename E> class Result {
 public:
 	// Tag types for constructor disambiguation
@@ -50,46 +47,4 @@ private:
 
 	std::optional<T> success_value;
 	std::optional<E> error_value;
-};
-
-// Specialization for void success type
-template <typename E> class Result<void, E> {
-public:
-	struct SuccessTag {};
-	struct ErrorTag {};
-
-	// Create successful result (no value needed for void)
-	static Result<void, E> success() {
-		return Result(SuccessTag{});
-	}
-
-	// Create error result with error
-	static Result<void, E> error(const E& error) {
-		return Result(ErrorTag{}, error);
-	}
-
-	// Check if result is successful
-	bool is_success() const {
-		return is_success_;
-	}
-
-	// Check if result is error
-	bool is_error() const {
-		return error_value.has_value();
-	}
-
-	// Get error value (returns std::nullopt if success)
-	std::optional<E> get_error() const {
-		return error_value;
-	}
-
-private:
-	// Private tagged constructors
-	Result(SuccessTag) : is_success_(true) {
-	}
-	Result(ErrorTag, const E& error) : error_value(error), is_success_(false) {
-	}
-
-	std::optional<E> error_value;
-	bool is_success_;
 };
