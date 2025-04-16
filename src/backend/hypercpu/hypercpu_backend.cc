f43a635601469b86f2758d5769badf60be354ce6
aceinetx <mouselin82@gmail.com>
2025-04-16 16:05:11 +0300

    Implement missing functions with filler ones in HyperCPU backend

--- src/backend/hypercpu/hypercpu_backend.cc
+++ src/backend/hypercpu/hypercpu_backend.cc
@@ -15,29 +15,34 @@ std::string HyperCPUBackend::emit_entrypoint() {
 )";
 }
 
-/*
 std::string HyperCPUBackend::emit_function_prologue() {
-	return "push bp\nmov bp sp\n";
+	return "";
 }
 
 std::string HyperCPUBackend::emit_function_epilogue() {
-	return "mov bp sp\npop bp\npop ip\n";
+	return "";
 }
 
 std::string HyperCPUBackend::emit_mov_const(int32_t constant) {
-	std::string code = fmt::format("movi r{} {}\n", reg_index, constant);
-	previous_reg_indexes.push(reg_index);
-	reg_index++;
-	if (reg_index > 12) {
-		reg_index = 0;
-	}
-	return code;
+	return "";
 }
 
-std::string HyperCPUBackend::emit_add(int32_t ROUT, int32_t RLHS, int32_t RRHS) {
-	return fmt::format("movi r{} r{}\nadd r{} r{}\n", ROUT, RLHS, ROUT, RRHS);
+std::string HyperCPUBackend::emit_add(std::string ROUT, std::string RLHS, std::string RRHS) {
+	return "";
 }
 
-std::string HyperCPUBackend::emit_mul(int32_t ROUT, int32_t RLHS, int32_t RRHS) {
-	return fmt::format("movi r{} r{}\nmul r{} r{}\n", ROUT, RLHS, ROUT, RRHS);
-}*/
+std::string HyperCPUBackend::emit_sub(std::string ROUT, std::string RLHS, std::string RRHS) {
+	return "";
+}
+
+std::string HyperCPUBackend::emit_mul(std::string ROUT, std::string RLHS, std::string RRHS) {
+	return "";
+}
+
+std::string HyperCPUBackend::emit_div(std::string ROUT, std::string RLHS, std::string RRHS) {
+	return "";
+}
+
+std::string HyperCPUBackend::emit_move(std::string rdest, std::string rsrc) {
+	return "";
+}
