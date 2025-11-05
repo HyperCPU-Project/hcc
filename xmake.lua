bf6e488bd761880fa651810d9a9294b961950506
aceinetx <mouselin82@gmail.com>
2025-11-05 10:21:56 +0300

    indentation

--- xmake.lua
+++ xmake.lua
@@ -8,25 +8,25 @@ set_languages("c++20")
 add_includedirs("src") -- includes for all targets
 
 target("hcc_core")
-    set_kind("static")
-    add_files(
-			"src/*.cc",
-			"src/ast/*.cc",
-			"src/backend/*/*.cc",
-			"src/backend/*.cc",
-			"src/value/*.cc",
-			"src/ir/*.cc"
-		)
-		add_files("src/ir/optimizations/*.cc")
+	set_kind("static")
+	add_files(
+		"src/*.cc",
+		"src/ast/*.cc",
+		"src/backend/*/*.cc",
+		"src/backend/*.cc",
+		"src/value/*.cc",
+		"src/ir/*.cc"
+	)
+	add_files("src/ir/optimizations/*.cc")
 
-		add_rules("lex", "yacc")
-		add_files("src/bison/*.ll", "src/bison/*.yy")
+	add_rules("lex", "yacc")
+	add_files("src/bison/*.ll", "src/bison/*.yy")
 target_end()
 
 
 target("hcc")
-    set_kind("binary")
-    add_files("src/main/main.cc")
-		add_deps("hcc_core")
-		add_packages("fmt")
+	set_kind("binary")
+	add_files("src/main/main.cc")
+	add_deps("hcc_core")
+	add_packages("fmt")
 target_end()
